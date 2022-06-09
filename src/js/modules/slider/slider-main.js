213ffc1fbc9076ff03d3c1cbaa317e3703562f4a
viktor <viktor.bulkin@stremline-c.com>
2022-06-09 15:21:36 +0300

    add more sliders

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -1,8 +1,8 @@
-import Slider from "./slider";
+import Slider from './slider';
 
 export default class MainSlider extends Slider {
-    constructor(page, btns) {
-        super(page, btns);
+    constructor(btns) {
+        super(btns);
     }
 
     showSlides(n) {
@@ -18,27 +18,31 @@ export default class MainSlider extends Slider {
             this.hanson.style.opacity = '0';
 
             if (n == 3) {
-                this.hanson.classList.add('animated')
+                this.hanson.classList.add('animated');
                 setTimeout(() => {
                     this.hanson.style.opacity = '1';
-                    this.hanson.classList.add('slideInUp')
-                }, 3000)
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
             } else {
-                this.hanson.classList.remove('slideInUp')
+                this.hanson.classList.remove('slideInUp');
             }
         } catch (e) { }
 
         this.slides.forEach(slide => {
             slide.style.display = 'none';
-        })
+        });
+
         this.slides[this.slideIndex - 1].style.display = 'block';
+    }
+
+    plusSlides(n) {
+        this.showSlides(this.slideIndex += n);
     }
 
     render() {
         try {
-            this.hanson = document.querySelector('.hanson')
+            this.hanson = document.querySelector('.hanson');
         } catch (e) { }
-
 
         this.btns.forEach(item => {
             item.addEventListener('click', () => {
